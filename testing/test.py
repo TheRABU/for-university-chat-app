d495d470875e1e77418518b0afb01660166ccc46
Maishaa-Zaman <138904408+Maishaa-Zaman@users.noreply.github.com>
2024-05-01 21:39:10 +0600

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -10,3 +10,10 @@ class ChatifyUnitTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome(service=Service(r"C:\Users\Mashu\Desktop\selenium-testing\chromedriver.exe"))
         self.addCleanup(self.browser.quit)
+        
+    def test1_homepage(self):
+        """Check if homepage is loaded properly."""
+        self.browser.get('http://localhost:5000')
+        sleep(3)
+        element = self.browser.find_element(By.XPATH, '/html/body/div/div/div[1]/div[1]/div[1]/a')
+        assert element.text == "Chat-iFy", "Homepage is not loaded properly."
