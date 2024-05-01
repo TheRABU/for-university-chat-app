cb238afecd8715cd2912da12bd164fbec14aad1f
Maishaa-Zaman <138904408+Maishaa-Zaman@users.noreply.github.com>
2024-05-01 21:39:56 +0600

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -17,3 +17,14 @@ class ChatifyUnitTest(unittest.TestCase):
         sleep(3)
         element = self.browser.find_element(By.XPATH, '/html/body/div/div/div[1]/div[1]/div[1]/a')
         assert element.text == "Chat-iFy", "Homepage is not loaded properly."
+
+    def test2_navbar_chat(self):
+        """Check if Login Page is loaded properly via the Nav-Bar."""
+        self.browser.get('http://localhost:5000')
+        sleep(3)
+        element = self.browser.find_element(By.XPATH, '/html/body/div/div/div[1]/div[1]/div[2]/ul/li[2]/a')
+        element.click()
+        sleep(3)
+        # assert "Login chat-iFy" in self.browser.page_source, "Login Page could not be accessed via the Nav-Bar"
+        login_button = self.browser.find_element(By.XPATH, '/html/body/div/div/div[2]/div/form/div[3]/button')
+        assert login_button is not None, "Login Page could not be accessed via the Nav-Bar"
